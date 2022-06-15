054403a891de74926e3ab8664e4bbc6acfcb67a5
Tristan Budiman <211120725@students.mikroskil.ac.id>
2022-06-15 11:14:15 +0700

    Add `Customer`

---
+++ Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aplikasi_struk
+{
+    class Customer
+    {
+        public string Name { get; }
+        public Customer(string name)
+        {
+        }
+    }
+}
