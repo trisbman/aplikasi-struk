e44d1a4a8c8665c1a6f0e117962a5ac18bacafca
Tristan Budiman <211120725@students.mikroskil.ac.id>
2022-06-15 11:12:49 +0700

    Add menu template

---
+++ Menu.cs
@@ -0,0 +1,56 @@
+﻿namespace aplikasi_struk
+{
+    class Dish
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public int Price { get; }
+        public Dish(int id, string name, int price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+    }
+    class Beverage : Dish
+    {
+        public Beverage(int Id, string BaseString, int Price) : base(Id, BaseString, Price)
+        {
+
+        }
+    }
+
+    class AddOn : Dish
+    {
+        public AddOn(int Id, string BaseString, int Price) : base(Id, BaseString, Price)
+        {
+
+        }
+    }
+    class Menu
+    {
+        public static string[] Category = {
+            "Makanan",
+            "Minuman",
+            "Lain-lain"
+        };
+
+        public static Dish[] MainDishes =
+        {
+            new Dish(1001, "Apple", 10000),
+            new Dish(1002, "Orange", 20000),
+        };
+
+        public static AddOn[] AddOns =
+        {
+            new AddOn(2001, "Udang", 5000),
+            new AddOn(2002, "Bakso", 2500)
+        };
+
+        public static Beverage[] Beverages =
+        {
+            new Beverage(3001, "Fanta", 15000),
+            new Beverage(3002, "Susu", 12500)
+        };
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,20 +2,26 @@
 
 namespace aplikasi_struk
 {
-    class Menu
+    struct Order
     {
-        public static string[] Category = { "Makanan", "Minuman", "Lain-lain" };
+        private int Id;
+        public int[] MealId;
+        private DateTime DateTime;
     }
     class Program
     {
-
         static Page page;
-
         static void Main(string[] args)
         {
             page = new Page("Dimsum Xpress", 50);
             page.WriteTitle("Selamat Datang di Dimsum Xpress Medan");
 
+            page.CreateList(Menu.Category);
+            int categoryIndex = page.AskInput("Masukkan pilihan : ") - 1;
+            Console.WriteLine("Pilihan Anda : " + Menu.Category[categoryIndex]);
+            
+            Console.ReadLine();
+            page.Clear();
 
 
             Console.ReadKey();
