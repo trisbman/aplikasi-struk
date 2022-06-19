c5f58dca538c1e8caf5c4f6dcc305da97f9f67a7
Tristan Budiman <211120725@students.mikroskil.ac.id>
2022-06-19 22:03:12 +0700

    FGS: Populate `Menu.cs`

--- Menu.cs
+++ Menu.cs
@@ -50,32 +50,65 @@
     {
         public static Dish[] MainDishes =
         {
-            new Dish(1001, "Apple", 10000),
-            new Dish(1002, "Orange", 20000),
+           new Dish(1001, "Pau Ayam Steam", 20800),
+            new Dish(1002, "Sio Mai Ayam", 21900),
+            new Dish(1003, "Sio Mai Rumput Laut", 26300),
+            new Dish(1004, "Sio Mai Jala-Jala", 21900),
+            new Dish(1005, "Sio Mai Udang", 21900),
+            new Dish(1006, "Sio Mai Kepiting", 21900),
+            new Dish(1007, "Kaki Ayam Saus Tiram", 21900),
+            new Dish(1008, "Ha Kaou Kepiting", 30700),
+            new Dish(1009, "Ha Kaou Udang", 30700),
+            new Dish(1010, "Lumpia Udang Kit Tahu", 30700),
+            new Dish(1011, "Hatosi Rambutan", 26300),
+            new Dish(1012, "Leng Hong Kien", 20700),
+            new Dish(1013, "Pangsit Udang Mayo", 30700),
+            new Dish(1014, "Kwotiek", 47200),
+            new Dish(1015, "Lumpia Udang Salad", 26300),
+            new Dish(1016, "Lumpia Ayam Rumput Laut", 21900),
+            new Dish(1017, "Ca Kue Isi Special", 21900),
+            new Dish(1018, "Lobak Ebi", 21900)
         };
 
         public static AddOn[] AddOns =
         {
-            new AddOn(2001, "Udang", 5000),
-            new AddOn(2002, "Bakso", 2500)
+            new AddOn(2001, "Telur Dadar", 7300),
+            new AddOn(2002, "Telur Mata Sapi", 7300),
+            new AddOn(2003, "Nasi Putih", 6000),
+            new AddOn(2004, "Nasi Prang", 8500)
         };
 
         public static Beverage[] Beverages =
         {
-            new Beverage(3001, "Fanta", 15000),
-            new Beverage(3002, "Susu", 12500)
+           new Beverage(3001, "Liang Teh 5 Rasa", 14200),
+            new Beverage(3002, "Orange Juice", 19700),
+            new Beverage(3003, "Ice Milk Tea", 19700),
+            new Beverage(3004, "Ice Lime Juice", 14200),
+            new Beverage(3005, "Lemon Tea", 14200),
+            new Beverage(3006, "Ice Green Tea", 19700),
+            new Beverage(3007, "Ice Jelly Lychee Tea", 17500),
+            new Beverage(3008, "Ice Jelly Coco Pandan", 14200),
+            new Beverage(3009, "Ice Jelly Java Tea", 14200),
+            new Beverage(3010, "Ice Tea Bunga", 14200),
+            new Beverage(3011, "Air Mineral", 5999)
         };
 
         public static Other[] Others =
         {
-            new Other(7001, "Es krim", 15000),
-            new Other(7002, "Bakpao", 12500)
+           new Other(7001, "Lapis Medan", 9600),
+            new Other(7002, "Puding Special", 13100),
+            new Other(7002, "Puding Kelapa Thai", 3000),
+            new Other(7002, "Pancake Avocado", 15700),
+            new Other(7002, "Pancake Durian", 18500),
+            new Other(7002, "Tako Ala Thai", 1200),
+            new Other(7002, "Puding Sunkist", 45000)
         };
 
         public static Category[] Categories = {
             new Category(MainDishes, "Makanan"),
             new Category(Beverages, "Minuman"),
             new Category(Others, "Lain-lain")
+
         };
 
     }
